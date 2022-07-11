fadc6f816bb4c81283c3cda266d4e829efe307d6
Prajwala Thatha Manjunatha <pthathamanjunatha@pthathamanjunathaMBP.local>
2022-07-11 12:16:32 -0700

    Add features:
    1. Add command line args: companies, difficulty
    2. Add get random question based on difficulty functionality

---
+++ src/args.rs
@@ -0,0 +1,34 @@
+use anyhow::anyhow;
+use anyhow::Error;
+use clap::Parser;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+}
+
+impl FromStr for Difficulty {
+    type Err = Error;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "EASY" => Ok(Difficulty::Easy),
+            "MEDIUM" => Ok(Difficulty::Medium),
+            "HARD" => Ok(Difficulty::Hard),
+            _ => Err(anyhow!(
+                "Invalid value: Available variants are: EASY, MEDIUM and HARD",
+            )),
+        }
+    }
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short, long, value_parser, default_value = "")]
+    pub companies: String,
+    #[clap(short, long, value_parser, default_value = "EASY")]
+    pub difficulty: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
+use crate::random_question::QuestionListFilterInput;
 use anyhow::anyhow;
 use anyhow::Error;
 use anyhow::Result as AnyhowResult;
+use clap::Parser;
 use graphql_client::{GraphQLQuery, Response};
 use reqwest;
+use std::str::FromStr;
+
+pub mod args;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/schema.graphql",
     query_path = "graphql/query.graphql",
-    response_derives = "Debug"
+    response_derives = "Debug, PartialEq"
 )]
 pub struct QuestionOfToday;
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/query.graphql",
+    response_derives = "Debug, PartialEq"
+)]
+pub struct RandomQuestion;
+
 const LEETCODE_GRAPHQL_ENDPOINT: &str = "https://leetcode.com/graphql";
 const LEETCODE_BASE_URL: &str = "https://leetcode.com";
+const LEETCODE_PROBLEMS_PREFIX: &str = "/problems/";
 
-async fn perform_query(
+async fn get_question_of_today(
     variables: question_of_today::Variables,
 ) -> Result<Response<question_of_today::ResponseData>, Error> {
     let request_body = QuestionOfToday::build_query(variables);
@@ -30,17 +44,52 @@ async fn perform_query(
     Ok(response_body)
 }
 
+async fn get_random_question(
+    variables: random_question::Variables,
+) -> Result<Response<random_question::ResponseData>, Error> {
+    let request_body = RandomQuestion::build_query(variables);
+    let client = reqwest::Client::new();
+    let res = client
+        .post(LEETCODE_GRAPHQL_ENDPOINT)
+        .json(&request_body)
+        .send()
+        .await?;
+    let response_body: Response<random_question::ResponseData> = res.json().await?;
+    Ok(response_body)
+}
+
 #[tokio::main]
 async fn main() -> AnyhowResult<(), Error> {
-    let variables = question_of_today::Variables;
+    //Read and validate command line args
+    let args = args::Args::parse();
+    let _difficulty = args::Difficulty::from_str(&args.difficulty)?;
 
-    let question_of_today = perform_query(variables)
+    let variables = question_of_today::Variables;
+    let question_of_today = get_question_of_today(variables)
         .await?
         .data
         .ok_or(anyhow!("Query failed"))?
         .active_daily_coding_challenge_question
         .link;
-    let fq_question_of_today = format!("{}{}", LEETCODE_BASE_URL, question_of_today);
-    println!("{:?}", fq_question_of_today);
+    let question_of_today_uri = format!("{}{}", LEETCODE_BASE_URL, question_of_today);
+    println!("{:?}", question_of_today_uri);
+
+    let variables_random = random_question::Variables {
+        category_slug: "".to_string(),
+        filters: QuestionListFilterInput {
+            difficulty: args.difficulty,
+        },
+    };
+    let random_question = get_random_question(variables_random)
+        .await?
+        .data
+        .ok_or(anyhow!("Random question query failed"))?
+        .random_question
+        .title_slug;
+    let random_question_uri = format!(
+        "{}{}{}",
+        LEETCODE_BASE_URL, LEETCODE_PROBLEMS_PREFIX, random_question
+    );
+    println!("{:?}", random_question_uri);
     Ok(())
 }
