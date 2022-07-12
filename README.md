354e92961bb427d45f50100822972082255d2b4d
Prajwala Thatha Manjunatha <pthathamanjunatha@pthathamanjunathaMBP.local>
2022-07-12 12:01:51 -0700

    Rust refactoring, basic readme added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # make-me-leetcode
-A leetcode bot written in Rust that pushes questions to a configured discord webhook 
+A leetcode bot written in Rust that pushes questions to a configured discord webhook
+
+Export DISCORD_WEBHOOK_URL environment variable:
+	export DISCORD_WEBHOOK_URL=<webhook_url_copied_from_channel_settings>
+
+cargo run -- --difficulty EASY
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use crate::random_question::QuestionListFilterInput;
 use anyhow::anyhow;
-use anyhow::Error;
 use anyhow::Result as AnyhowResult;
 use clap::Parser;
 use graphql_client::{GraphQLQuery, Response};
@@ -34,7 +33,7 @@ const DISCORD_WEBHOOK_URL: &str = "DISCORD_WEBHOOK_URL";
 
 async fn make_post_query<T: GraphQLQuery>(
     variables: T::Variables,
-) -> Result<Response<T::ResponseData>, Error> {
+) -> AnyhowResult<Response<T::ResponseData>> {
     let request_body = T::build_query(variables);
     let client = reqwest::Client::new();
     let res = client
@@ -47,7 +46,7 @@ async fn make_post_query<T: GraphQLQuery>(
     Ok(response_body)
 }
 
-async fn fetch_and_post_question_of_today() -> AnyhowResult<(), Error> {
+async fn fetch_and_post_question_of_today() -> AnyhowResult<()> {
     let variables = question_of_today::Variables;
     let question_of_today = make_post_query::<QuestionOfToday>(variables)
         .await?
@@ -56,13 +55,12 @@ async fn fetch_and_post_question_of_today() -> AnyhowResult<(), Error> {
         .active_daily_coding_challenge_question
         .link;
     let question_of_today_uri = format!("{}{}", LEETCODE_BASE_URL, question_of_today);
-    println!("{:?}", question_of_today_uri);
+    post_to_discord(&question_of_today_uri).await;
 
-    post_to_discord(&question_of_today_uri).await?;
     Ok(())
 }
 
-async fn fetch_and_post_random_question(difficulty: String) -> AnyhowResult<(), Error> {
+async fn fetch_and_post_random_question(difficulty: String) -> AnyhowResult<()> {
     let variables_random = random_question::Variables {
         category_slug: "".to_string(),
         filters: QuestionListFilterInput { difficulty },
@@ -78,33 +76,40 @@ async fn fetch_and_post_random_question(difficulty: String) -> AnyhowResult<(), 
         "{}{}{}",
         LEETCODE_BASE_URL, LEETCODE_PROBLEMS_PREFIX, random_question
     );
-    println!("{:?}", random_question_uri);
+    post_to_discord(&random_question_uri).await;
 
-    post_to_discord(&random_question_uri).await?;
     Ok(())
 }
 
-async fn post_to_discord(message: &str) -> AnyhowResult<(), Error> {
+async fn post_to_discord(message: &str) {
     //Read discord webhook url
     let discord_webhook_url = env::var(DISCORD_WEBHOOK_URL).unwrap();
     let token = discord_webhook_url.split("/").last().unwrap();
 
     let http = Http::new(token);
-    let webhook = http.get_webhook_from_url(&discord_webhook_url).await?;
+
+    //Panic if anything related to discord fails - fetching webhook from url, posting to channel
+    //etc.
+
+    let webhook = http
+        .get_webhook_from_url(&discord_webhook_url)
+        .await
+        .expect("Getting webhook from url failed; Verify if the url is correct!");
+
     webhook
         .execute(&http, false, |w| w.content(message))
-        .await?;
-    Ok(())
+        .await
+        .expect("Posting to discord failed");
 }
 
 #[tokio::main]
-async fn main() -> AnyhowResult<(), Error> {
+async fn main() -> AnyhowResult<()> {
     //Read and validate command line args
     let args = args::Args::parse();
     let _difficulty = args::Difficulty::from_str(&args.difficulty)?;
 
     let fut_question_of_today = fetch_and_post_question_of_today();
     let fut_random_question = fetch_and_post_random_question(args.difficulty);
-    let (_res1, _res2) = tokio::join!(fut_question_of_today, fut_random_question);
+    tokio::try_join!(fut_question_of_today, fut_random_question)?;
     Ok(())
 }
